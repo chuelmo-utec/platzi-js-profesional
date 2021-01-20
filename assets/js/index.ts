0fa9753de86e2c7b3b3e399045a11497e989f3e3
chuelmo-utec <christian.huelmo@estudiantes.utec.edu.uy>
2021-01-20 09:06:54 -0300

    Agregamos el AdsPlugin que pega una pequeña publicidad en la parte de abajo del video al estilo youtube

--- assets/js/index.ts
+++ assets/js/index.ts
@@ -1,13 +1,15 @@
 import MediaPlayer from './MediaPlayer'
 import AutoPlay from './plugins/AutoPlay'
 import AutoPause from './plugins/AutoPause'
+import AdsPlugin from './plugins/Ads/AdsPlugin'
 
 const video = document.querySelector('video')
 const playButton: HTMLElement = document.getElementById('playButton')
 const muteButton: HTMLElement = document.getElementById('muteButton')
 const player = new MediaPlayer({ el: video, plugins: [
     new AutoPlay(),
-    new AutoPause()
+    new AutoPause(),
+    new AdsPlugin(),
 ] })
 playButton.onclick = () => player.togglePlay(playButton)
 muteButton.onclick = () => player.toggleMute(muteButton)
